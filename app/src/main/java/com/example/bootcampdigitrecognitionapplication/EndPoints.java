94a6c8a5f5e8e2865a1cb7bfaa2325910592ea25
rashtell <rashtell2@gmail.com>
2019-11-21 01:17:58 +0100

    first commit: this uses a trained model to recognize numbers and letters in a picture

---
+++ app/src/main/java/com/example/bootcampdigitrecognitionapplication/EndPoints.java
@@ -0,0 +1,5 @@
+package com.example.bootcampdigitrecognitionapplication;
+
+public class EndPoints {
+    public static final String PREDICT_URL = "http://ec2-3-14-5-1.us-east-2.compute.amazonaws.com:5000/model/predict";
+}
